0089def4d86f1a9af3a74a9192e664b20cab322c
Raphael Rychard <67989564+RaphaelRychard@users.noreply.github.com>
2022-11-22 16:24:47 -0400

    add

---
+++ TelaFuncoa01/src/telafuncoa02/TelaFuncoa02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package telafuncoa02;
+
+/**
+ *
+ * @author rrych
+ */
+public class TelaFuncoa02 {
+
+    static void soma (int a, int b) {
+        int s = a + b;
+        System.out.println("A soma é " + s);
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Resultado");
+        soma(2, 4);
+        
+    }
+    
+}
---
+++ TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefuncao01;
+
+public class TesteFuncao01 {
+    
+    static void soma (int a, int b) {
+        int s = a + b;
+        System.out.println("A soma e " + s);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("A soma e ");
+        soma (4, 3);
+    }
+    
+}
