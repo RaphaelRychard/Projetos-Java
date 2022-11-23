c0da83e2e234b22fc324e72f55a48b95862ade12
Raphael Rychard <67989564+RaphaelRychard@users.noreply.github.com>
2022-11-23 16:18:17 -0400

    add

---
+++ TesteFuncao03/src/testefuncao03/TesteFuncao03.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefuncao03;
+
+/**
+ *
+ * @author rrych
+ */
+public class TesteFuncao03 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.err.println("Vai começar a contagem");
+        
+        System.out.println(Operacoes.contador(1, 10));
+    }
+}
---
+++ teste/src/teste/Teste.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste;
+
+/**
+ *
+ * @author rrych
+ */
+public class Teste {
+
+public static void main(String[] args) {
+ 
+    int x, y;
+    double z;
+    x = 5;
+    y = 2;
+    z = x / y;
+    System.out.print(z);
+
+}
+}
